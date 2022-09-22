3a217d76aedf2f8bdc64b1523980f0060cb83202
Aben25 <51212800+Aben25@users.noreply.github.com>
2022-09-22 15:36:57 -0400

    photo

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -8,10 +8,12 @@ export default function Confirmation() {
 
   return (
       <div className="container mt-5">
-        <img className='mb-3' src="./checked.png" width="25%" alt="ArtBA Logo" />
+        <img className='mb-3' src="./checked.png" width="15%" alt="ArtBA Logo" />
 
           <h3>{hit?.FullName} have been successfully checked-in!</h3>
           <p>Please grab your badge at the registration desk.</p>
+          <img src="./reg.jpg" width="30%" className='rounded' alt="" />
+          <br/>
           <a
           onClick={() => navigate("/")}
               href="#" className="btn btn-success mt-3">Start a New Check In</a>
